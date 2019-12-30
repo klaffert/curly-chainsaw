faad04f6439b30d53e70d83ae3aba9d27f33c796
Kelliann Lafferty <lafferty.kelliann@gmail.com>
2019-12-30 07:51:32 -0800

    Using Refs with React Hooks

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,15 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import classes from "./Cockpit.css";
 
 const cockpit = props => {
+  const toggleBtnRef = useRef(null)
+
   useEffect(() => {
     console.log("[Cockpit.js] useEffect");
     // HTTP request...
     // Can use useEffect as much as you want
     // Combines componentDidMount and componentDidUpdate in functional components
-    setTimeout(() => {
-      alert("Saved data to cloud!");
-    }, 1000);
+    // setTimeout(() => {
+    //   alert("Saved data to cloud!");
+    // }, 1000);
+    toggleBtnRef.current.click()
     return () => {
       console.log("[Cockpit.js] cleanup work in useEffect");
     };
@@ -39,7 +42,7 @@ const cockpit = props => {
     <div className={classes.Cockpit}>
       <h1>{props.title}</h1>
       <p className={assignedClasses.join(" ")}>This is really working!</p>
-      <button className={btnClass} onClick={props.clicked}>
+      <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
         Toggle Name:
       </button>
     </div>
@@ -48,5 +51,5 @@ const cockpit = props => {
 
 export default React.memo(cockpit);
 
-// Good idea to use React.memo() for functional components that might not 
-// have to update with each change in Parent component with it 
+// Good idea to use React.memo() for functional components that might not
+// have to update with each change in Parent component with it
