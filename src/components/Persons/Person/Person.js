da980b6243bd816c0d16911b97a1218b5b3a973e
Kelliann Lafferty <lafferty.kelliann@gmail.com>
2019-12-30 07:33:57 -0800

    Using PropTypes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import PropTypes, { string } from 'prop-types';
+
 import Aux from "../../../hoc/Aux";
 import withClass from "../../../hoc/withClass";
 import classes from "./Person.css";
@@ -10,7 +12,7 @@ class Person extends Component {
     return (
       <Aux>
         <p onClick={this.props.click}>
-          I'm {this.props.name} and I am {this.props.age} years old!{" "}
+          I'm {this.props.name} and I am {this.props.age} years old!
         </p>
         <p key="i2"> {this.props.children} </p>
         <input
@@ -22,6 +24,13 @@ class Person extends Component {
       </Aux>
     );
   }
+}
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
 }
 
 export default withClass(Person, classes.Person);
